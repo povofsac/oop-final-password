1662a9b03c069e2033ddd2e187c23cfe4679de58
povofsac <125424770+povofsac@users.noreply.github.com>
2023-06-19 10:14:35 +0545

    Update PasswordStrengthChecker.h

--- PasswordStrengthChecker.h
+++ PasswordStrengthChecker.h
@@ -10,55 +10,6 @@ class PasswordStrengthChecker {
 private:
       string password;
 public:
-    int getPasswordLength(int strengthLevel) {
-        int length;
-        switch (strengthLevel) {
-            case 1:
-                length = 6;
-                break;
-            case 2:
-                length = 10;
-                break;
-            case 3:
-                length = 14;
-                break;
-            default:
-                length = 0;
-        }
-        return length;
-    }
-
-    string generatePassword(int length, int strengthLevel) {
-        string password;
-        string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-        string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string digitChars = "0123456789";
-        string specialChars = "!@#$%^&*()";
-
-        string characters;
-
-        characters += lowercaseChars;
-        characters += uppercaseChars;
-
-        if (strengthLevel >= 2) {
-            characters += digitChars;
-        }
-
-        if (strengthLevel >= 3) {
-            characters += specialChars;
-        }
-
-        random_device rd;
-        mt19937 generator(rd());
-        uniform_int_distribution<int> distribution(0, characters.length() - 1);
-
-        for (int i = 0; i < length; i++) {
-            int index = distribution(generator);
-            password += characters[index];
-        }
-
-        return password;
-    }
 
     PasswordStrengthChecker(const string& pass) : password(pass) {}
 
@@ -82,11 +33,14 @@ public:
         regex uppercase("[A-Z]");
         regex lowercase("[a-z]");
         regex digit("[0-9]");
+        regex sp_chars("[!@#$%^&*(),.?:{}|<>]");
 
         if (password.length() < 10||
         !regex_search(password, uppercase)||
         !regex_search(password, lowercase)||
-        !regex_search(password, digit))
+        !(
+            regex_search(password, digit)) || regex_search(password, sp_chars)
+        )
             return false;
 
         return true;
